6ab9a5129e59f7c2584c55aff38734f53d267a80
christopherkim2273 <christopher2273@gmail.com>
2022-04-14 21:31:33 -0400

    Added COmments

--- src/com/company/ScheduleBuilder/UI/Application.java
+++ src/com/company/ScheduleBuilder/UI/Application.java
@@ -2,3 +2,28 @@ package com.company.ScheduleBuilder.UI;
 
 public class Application {
 }
+
+// Static dictionary - {Sunday,Monday...}
+// String Day;
+
+// Schedule Class -> ArrayList<DayOfWeek> -> 0 ( DayOfWeek.Day .equals Sunday) ..
+
+// DayOfWeek Class -> Monday Objects
+
+//Classes for Day Of The Week
+    // DayOFWeek -> Times, Hours, Classes, Events.
+    // Monday <-> Tuesday (Same fields) Monday extend DayOfWeek
+    // Schedule
+// Algorithm For Finding Free Time -> Compare All Corresponding DayOfWeek Objects <-> Monday Objects
+
+// Class Groups -> Form groups
+
+// DataBase (MongoDB)
+
+// Class to Import from Google Calendar
+
+// Class To Add Custom Events/Classes
+
+// UI for all classes.
+
+
