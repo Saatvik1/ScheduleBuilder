6e27eeb200033c5ccf85ffad1257b3f84a051156
saatvik <saatviks3723@gmail.com>
2022-04-14 21:51:54 -0400

    Event stuff

--- src/com/company/ScheduleBuilder/Schedules/Events/Event.java
+++ src/com/company/ScheduleBuilder/Schedules/Events/Event.java
@@ -1,4 +1,45 @@
 package com.company.ScheduleBuilder.Schedules.Events;
 
 public class Event {
+    private String name;
+    private String[] timeStart = {"Days","Hour", "Minute", "AMPM"};
+    private String description;
+    private String tags;
+
+    public Event (String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getTimeStart() {
+        return timeStart;
+    }
+
+    public void setTimeStart(String[] timeStart) {
+        this.timeStart = timeStart;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
 }
